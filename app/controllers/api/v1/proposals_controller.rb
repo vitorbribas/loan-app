f94265219306ef279b9eef5db09a6c9eae856494
Vítor Ribas <bsbribas66@gmail.com>
2024-12-09 23:31:48 -0300

    Adiciona logs ao retornar uma simulação de proposta

--- app/controllers/api/v1/proposals_controller.rb
+++ app/controllers/api/v1/proposals_controller.rb
@@ -7,10 +7,12 @@ class Api::V1::ProposalsController < ApplicationController
 
     if id.present?
       @proposal = Proposal.find_by(id: id)
+      log_proposal
       return render :create, status: :ok
     end
 
     if @proposal.save
+      log_proposal
       render :create, status: :created
     else
       render :errors, status: :unprocessable_entity
@@ -21,5 +23,9 @@ class Api::V1::ProposalsController < ApplicationController
 
   def proposal_params
     params.require(:proposal).permit(:amount, :payment_term, :birthdate, :email)
+  end
+
+  def log_proposal
+    Rails.logger.info("##### Proposal created/found: #{@proposal.attributes}")
   end
 end
